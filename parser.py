e37934de5f5c845fb843ef2fb7e531c750e86715
Geoffrey Wu <geoffrey.feifei@gmail.com>
2022-06-18 20:33:20 -0400

    refactored code for readability

--- parser.py
+++ parser.py
@@ -3,18 +3,28 @@ import os
 import regex
 import time
 
-has_category_tags = True
+HAS_CATEGORY_TAGS = True
 
-input_directory = 'packets/'
-output_directory = 'output/'
-os.mkdir(output_directory)
+INPUT_DIRECTORY = 'packets/'
+OUTPUT_DIRECTORY = 'output/'
+os.mkdir(OUTPUT_DIRECTORY)
 
+
+REGEX_QUESTION = r'^\d{1,2}\.(?:.|\n)*?ANSWER:(?:.*\n)*?(?=\d{1,2}\.)'
+REGEX_CATEGORY_TAG = r'<.*?>'
+
+REGEX_TOSSUP_TEXT = r'(?<=\d{1,2}\.)(?:.|\n)*?(?=ANSWER)'
+REGEX_TOSSUP_ANSWER = r'(?<=ANSWER:)(?:.|\n)*(?=<)' if HAS_CATEGORY_TAGS else r'(?<=ANSWER:)(?:.|\n)*'
+
+REGEX_BONUS_LEADIN = r'(?<=\d{1,2}\.)(?:.|\n)*?(?=\[)'
+REGEX_BONUS_PARTS = r'(?<=\[(?:10)?[EMH]?\])(?:.|\n)*?(?=ANSWER)'
+REGEX_BONUS_ANSWERS = r'(?<=ANSWER:)(?:.|\n)*?(?=\[(?:10)?[EMH]?\]|<)'
 
 with open('subcat_conversion.json') as f:
     subcat_conversion = json.load(f)
 
 with open('subcat.json') as f:
-    subcat = json.load(f)
+    cat_conversion = json.load(f)
 
 with open('stop_words.txt') as f:
     stop_words = set(f.readlines())
@@ -24,8 +34,7 @@ with open('stop_words.txt') as f:
 all_questions = []
 for (dirpath, dirnames, filenames) in os.walk('packets_classify'):
     for filename in filenames:
-        if filename == '.DS_Store':
-            continue
+        if filename == '.DS_Store': continue
 
         f = open(dirpath + '/' + filename)
 
@@ -56,9 +65,9 @@ def get_subcategory(s):
 
 
 def difference(text1, text2):
-    """
-    Returns the number of words in common between the two words
-    """
+    '''
+    Returns the number of words in common between the two strings
+    '''
     count = 0
 
     words1 = text1
@@ -69,7 +78,7 @@ def difference(text1, text2):
         if word in words2: count += len(word)
 
     return count
- 
+
 
 def classify_question(text, is_tossup, max_number_to_check=100000):
     i = 0
@@ -102,26 +111,25 @@ def classify_question(text, is_tossup, max_number_to_check=100000):
     return best['category'], best['subcategory']
 
 
-for file in os.listdir(input_directory):
+for file in os.listdir(INPUT_DIRECTORY):
     if file == '.DS_Store': continue
 
-    f = open(input_directory + file)
-    g = open(output_directory + file[:-4] + '.json', 'w')
+    f = open(INPUT_DIRECTORY + file)
+    g = open(OUTPUT_DIRECTORY + file[:-4] + '.json', 'w')
 
     data = {
-        "tossups": [],
-        "bonuses": []
+        'tossups': [],
+        'bonuses': []
     }
 
     packet_text = ''
     for line in f.readlines():
         packet_text += line
-    
-    packet_text = packet_text + '\n0.'
 
+    packet_text = packet_text + '\n0.'
     packet_text = packet_text.replace('', '')
     packet_text = regex.sub(r'ten\spoints', '10 points', packet_text)
-    packet_questions = regex.findall(r'^\d{1,2}\.(?:.|\n)*?ANSWER:(?:.*\n)*?(?=\d{1,2}\.)', packet_text, flags=regex.IGNORECASE | regex.MULTILINE)
+    packet_questions = regex.findall(REGEX_QUESTION, packet_text, flags=regex.IGNORECASE | regex.MULTILINE)
 
     tossups = []
     bonuses = []
@@ -135,70 +143,67 @@ for file in os.listdir(input_directory):
     print(f'{file}: parsed {len(tossups)} tossups and {len(bonuses)} bonuses')
 
     for i, tossup in enumerate(tossups):
-        question = regex.findall(r'(?<=\d{1,2}\.)(?:.|\n)*?(?=ANSWER)', tossup, flags=regex.IGNORECASE)[0].strip().replace('\n', ' ')
-        data['tossups'].append({"question": question})
+        question = regex.findall(REGEX_TOSSUP_TEXT, tossup, flags=regex.IGNORECASE)[0].strip().replace('\n', ' ')
+        data['tossups'].append({'question': question})
 
-        answer = regex.findall(r'(?<=ANSWER:)(?:.|\n)*(?=<)' if has_category_tags else r'(?<=ANSWER:)(?:.|\n)*', tossup, flags=regex.IGNORECASE)[0].strip().replace('\n', ' ')
+        answer = regex.findall(REGEX_TOSSUP_ANSWER, tossup, flags=regex.IGNORECASE)[0].strip().replace('\n', ' ')
         data['tossups'][i]['answer'] = answer
 
-        if has_category_tags:
-            j = regex.findall(r'<.*?>', tossup, flags=regex.IGNORECASE)[0].strip().replace('\n', ' ')
+        if HAS_CATEGORY_TAGS:
+            j = regex.findall(REGEX_CATEGORY_TAG, tossup, flags=regex.IGNORECASE)[0].strip().replace('\n', ' ')
             cat = get_subcategory(j)
             if len(cat) == 0:
-                print(i+1, "tossup error finding the subcategory", j)
+                print(i+1, 'tossup - error finding the subcategory', j)
             else:
                 data['tossups'][i]['subcategory'] = cat
-                data['tossups'][i]['category'] = subcat[cat]
+                data['tossups'][i]['category'] = cat_conversion[cat]
 
     for i, bonus in enumerate(bonuses):
-        leadin = regex.findall(r'(?<=\d{1,2}\.)(?:.|\n)*?(?=\[)', bonus, flags=regex.IGNORECASE)[0].strip().replace('\n', ' ')
-        data['bonuses'].append({"leadin": leadin})
+        leadin = regex.findall(REGEX_BONUS_LEADIN, bonus, flags=regex.IGNORECASE)[0].strip().replace('\n', ' ')
+        data['bonuses'].append({'leadin': leadin})
+
+        data['bonuses'][i]['parts'] = []
+        parts = regex.findall(REGEX_BONUS_PARTS, bonus, flags=regex.IGNORECASE)
+        for part in parts:
+            part = part.strip().replace('\n', ' ')
+            data['bonuses'][i]['parts'].append(part)
 
         bonus = bonus + '\n[10]'
-        data['bonuses'][i]["answers"] = []
-        answers = regex.findall(r'(?<=ANSWER:)(?:.|\n)*?(?=\[(?:10)?[EMH]?\]|<)', bonus, flags=regex.IGNORECASE)
+        data['bonuses'][i]['answers'] = []
+        answers = regex.findall(REGEX_BONUS_ANSWERS, bonus, flags=regex.IGNORECASE)
         for answer in answers:
             answer = answer.strip().replace('\n', ' ')
-            data['bonuses'][i]["answers"].append(answer)
+            data['bonuses'][i]['answers'].append(answer)
         bonus = bonus[:-5]
 
-        data['bonuses'][i]["parts"] = []
-        parts = regex.findall(r'(?<=\[(?:10)?[EMH]?\])(?:.|\n)*?(?=ANSWER)', bonus, flags=regex.IGNORECASE)
-        for answer in parts:
-            answer = answer.strip().replace('\n', ' ')
-            data['bonuses'][i]["parts"].append(answer)
-        
-        if has_category_tags:
-            j = regex.findall(r'<.*?>', bonus, flags=regex.IGNORECASE)[0].strip().replace('\n', ' ')
+        if HAS_CATEGORY_TAGS:
+            j = regex.findall(REGEX_CATEGORY_TAG, bonus, flags=regex.IGNORECASE)[0].strip().replace('\n', ' ')
             cat = get_subcategory(j)
             if len(cat) == 0:
-                print(i+1, "bonus error finding the subcategory", j)
+                print(i+1, 'bonus - error finding the subcategory', j)
             else:
                 data['bonuses'][i]['subcategory'] = cat
-                data['bonuses'][i]['category'] = subcat[cat]
+                data['bonuses'][i]['category'] = cat_conversion[cat]
 
     time_now = time.perf_counter()
 
-    if not has_category_tags:
-        for bonus in data['bonuses']:
-            if 'parts' not in bonus:
-                print('No parts for bonus', bonus)
-                continue
-            if len(bonus['parts']) < 3:
-                print('Bonus', bonus['parts'], 'has less than 3 parts')
-                continue
-            category, subcategory = classify_question(bonus['leadin'] + bonus['parts'][0] + bonus['parts'][1] + bonus['parts'][2], is_tossup=False)
-            bonus['category'] = category
-            bonus['subcategory'] = subcategory
-
-            # print(category, subcategory)
-
+    if not HAS_CATEGORY_TAGS:
         for tossup in data['tossups']:
             category, subcategory = classify_question(tossup['question'], is_tossup=True)
             tossup['category'] = category
             tossup['subcategory'] = subcategory
 
-            # print(category, subcategory)
+        for bonus in data['bonuses']:
+            if 'parts' not in bonus:
+                print('No parts found for bonus', bonus)
+                continue
+            if len(bonus['parts']) < 3:
+                print('Bonus', bonus['parts'], 'has fewer than 3 parts')
+                continue
+            category, subcategory = classify_question(bonus['leadin'] + bonus['parts'][0] + bonus['parts'][1] + bonus['parts'][2], is_tossup=False)
+            bonus['category'] = category
+            bonus['subcategory'] = subcategory
 
         print(f'it took {time.perf_counter() - time_now} seconds to classify')
+
     json.dump(data, g)
