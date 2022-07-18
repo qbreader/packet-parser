838d1cbb8693e32437f445744559b1991204c453
Geoffrey Wu <42471355+geoffrey-wu@users.noreply.github.com>
2022-07-18 15:50:32 -0400

    preliminary answerline parsing

---
+++ answerline.py
@@ -0,0 +1,113 @@
+from collections import Counter
+import json
+import re
+
+def get_indicator(text):
+    indicators = ['']
+    text = text.lower()
+    text = text.split(' ')
+    for i in range(len(text) - 1):
+        if text[i] in ['this', 'these']:
+            indicator = text[i+1]
+            if indicator[-2:] == '\'s':
+                indicator = indicator[:-2]
+            if indicator[-1:] in ['\'', ',', '.']:
+                indicator = indicator[:-1]
+
+            indicators.append(indicator)
+
+    return Counter(indicators).most_common(1)[0][0]
+
+f = open('packet.json')
+data = json.load(f)
+
+questions = data['tossups']
+
+for question in questions:
+    answer_formatted = ''
+    index = question['answer'].find('[')
+    if index == -1:
+        main_answer = question['answer'].strip()
+        print(f"{get_indicator(question['question'])}: {main_answer}")
+        continue
+
+    acceptable = []
+    promptable = []
+    rejectable = []
+    main_answer = question['answer'][:index].strip()
+    answer_formatted += f'<b><u>{main_answer}</u></b> ['
+    alternate_answer = question['answer'][index + 1:question['answer'].find(']')]
+    answer_fragments = alternate_answer.split(';')
+    for fragment in answer_fragments:
+        fragment = fragment.strip()
+        if fragment[:2] == 'or': acceptor = 'or'
+        if fragment[:6] == 'accept': acceptor = 'accept'
+        if fragment[:2] == 'or' or fragment[:6] == 'accept':
+            fragment = ' ' + fragment
+            fragment = re.split(' or | accept ', fragment)
+            first = False
+            for i, piece in enumerate(fragment):
+                piece = piece.strip()
+                if len(piece) == 0: continue
+                if piece == 'equivalents': 
+                    answer_formatted += f' {acceptor} equivalents'
+                    continue
+                if piece.find('before') != -1:
+                    before, after = piece[:piece.index('before')], piece[piece.index('before') + 6:]
+                    before = before.strip()
+                    after = after.strip()
+                    answer_formatted += f'accept <u>{before}</u> before {after}'
+                    continue
+                if not first:
+                    answer_formatted += f'{acceptor} <b><u>{piece}</u></b>'
+                    first = True
+                else:
+                    answer_formatted += f' or <b><u>{piece}</u></b>'
+                acceptable.append(piece)
+            answer_formatted += f'; '
+
+        elif fragment[:9] == 'prompt on':
+            fragment = ' ' + fragment
+            fragment = re.split(' prompt on | or ', fragment)
+            first = False
+            for piece in fragment:
+                piece = piece.strip()
+                if len(piece) == 0: continue
+                if piece == 'equivalents': continue
+                if piece.find('before') != -1:
+                    before, after = piece[:piece.index('before')], piece[piece.index('before') + 6:]
+                    before = before.strip()
+                    after = after.strip()
+                    answer_formatted += f'prompt on <u>{before}</u> before {after}'
+                    continue
+                if not first:
+                    answer_formatted += f'prompt on <u>{piece}</u>'
+                    first = True
+                else:
+                    answer_formatted += f' or <u>{piece}</u>'
+                promptable.append(piece)
+            answer_formatted += '; '
+        elif fragment[:6] == 'reject':
+            answer_formatted += f'{fragment}; '
+            fragment = fragment[6:].strip()
+            if fragment[0] == '"':
+                fragment = fragment[1:]
+            if fragment[-1] == '"':
+                fragment = fragment[:-1]
+            rejectable.append(fragment)
+        elif fragment[:26] == 'do not accept or prompt on':
+            answer_formatted += f'{fragment}; '
+            fragment = fragment[26:].strip()
+            if fragment[0] == '"':
+                fragment = fragment[1:]
+            if fragment[-1] == '"':
+                fragment = fragment[:-1]
+            rejectable.append(fragment)
+        else:
+            answer_formatted += f'{fragment}; '
+    answer_formatted = answer_formatted[:-2] + ']'
+    print(f"{get_indicator(question['question'])}: {main_answer} [{alternate_answer}]")
+    print(acceptable)
+    print(promptable)
+    print(rejectable)
+    print(answer_formatted)
