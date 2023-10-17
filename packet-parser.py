51645e687b8cc195c3b0333d409e72568cf2446c
Geoffrey Wu <geoffreywu1000@gmail.com>
2023-10-17 14:34:35 -0400

    also split by semicolon

--- packet-parser.py
+++ packet-parser.py
@@ -52,7 +52,7 @@ def get_subcategory(text: str) -> str:
     text = text.lower()
     text = text.replace("–", " ")
     text = text.replace("-", " ")
-    text_split = regex.split(r"[\/, ]", text)
+    text_split = regex.split(r"[\/,; ]", text)
 
     for subcat in STANDARDIZE_SUBCATS:
         works = True
@@ -74,7 +74,7 @@ def get_alternate_subcategory(text: str) -> str:
     text = text.lower()
     text = text.replace("–", " ")
     text = text.replace("-", " ")
-    text_split = regex.split(r"[\/, ]", text)
+    text_split = regex.split(r"[\/,; ]", text)
 
     for subcat in STANDARDIZE_ALTERNATE_SUBCATS:
         works = True
