38c782d44be988f90a04c7bb2a5dc370c148c078
Geoffrey Wu <geoffreywu1000@gmail.com>
2024-05-01 20:15:44 -0400

    grab text from tables

--- modules/docx-to-txt.py
+++ modules/docx-to-txt.py
@@ -1,12 +1,12 @@
 # Converts a docx file to a txt file while annotating the bold, italic, and underlined parts (and no other styling).
 
-from docx import Document
+import docx
 import sys
 
-document = Document(sys.argv[1])
-for para in document.paragraphs:
+
+def parse_paragraph(paragraph: docx.text.paragraph.Paragraph) -> str:
     text = ""
-    for runs in para.runs:
+    for runs in paragraph.runs:
         if len(runs.text.strip()) == 0:
             text += runs.text
             continue
@@ -24,4 +24,18 @@ for para in document.paragraphs:
 
         text += run
 
-    print(text)
+    return text
+
+
+file = sys.argv[1]
+document = docx.Document(file)
+
+for item in document.iter_inner_content():
+    if isinstance(item, docx.text.paragraph.Paragraph):
+        print(parse_paragraph(item))
+        continue
+    else:
+        for row in item.rows:
+            for cell in row.cells:
+                for paragraph in cell.paragraphs:
+                    print(parse_paragraph(paragraph))
