269be1748e6f1e6d8904d1151fa7bb77bbdee599
Geoffrey Wu <geoffreywu1000@gmail.com>
2025-07-05 18:23:35 -0500

    replace non-breaking spaces with normal spaces

--- packet-parser.py
+++ packet-parser.py
@@ -719,8 +719,8 @@ class Parser:
             packet_text = packet_text.replace(typo, "ANSWER:")
             packet_text = packet_text.replace(typo.title(), "ANSWER:")
 
-        # replace tabs and redundant spaces
-        packet_text = packet_text.replace("\t", " ")
+        # replace tabs, non-breaking spaces (nbsp), and redundant spaces
+        packet_text = packet_text.replace("\t", " ").replace("\xa0", " ")
         packet_text = regex.sub(r" {2,}", " ", packet_text, flags=Parser.REGEX_FLAGS)
 
         # remove redundant tags
